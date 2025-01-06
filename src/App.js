36bc5c65182ac3f67375689bccb1299873ea40af
Rani <swati.rani@capgemini.com>
2025-01-06 23:03:45 +0530

    Initial commit - Customer rewards program

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import MainComponent from './Main';
+function App() {
+  return (
+    <div className="App">
+      <MainComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Rewards.js
@@ -0,0 +1,34 @@
+export const calculatePoints = (amount) => {
+    let points = 0;
+    if (amount > 100) {
+      points += 2 * (amount - 100);
+      amount = 100;
+    }
+    if (amount > 50) {
+      points += 1 * (amount - 50);
+    }
+    return points;
+  };
+  
+  export const groupByCustomerAndMonth = (transactions) => {
+    const groupedData = {};
+  
+    transactions.forEach(({ customer, amount, date }) => {
+      const month = new Date(date).toLocaleString("default", { month: "long" });
+      const points = calculatePoints(amount);
+  
+      if (!groupedData[customer]) {
+        groupedData[customer] = {};
+      }
+  
+      if (!groupedData[customer][month]) {
+        groupedData[customer][month] = { points: 0, transactions: [] };
+      }
+  
+      groupedData[customer][month].points += points;
+      groupedData[customer][month].transactions.push(amount);
+    });
+  
+    return groupedData;
+  };
+  
